207b427c801d8fe3b73349046141ab14198c1e26
Andreas Brandl <abrandl@gitlab.com>
2020-12-29 20:34:05 +0100

    SAVEPOINT

--- docker/dblab-ssh/entrypoint.sh
+++ docker/dblab-ssh/entrypoint.sh
@@ -8,14 +8,20 @@ echo "${DBLAB_HOST_KEYS}" > ~/.ssh/known_hosts
 
 cat ~/.ssh/known_hosts
 
-ssh -f -N -L 2344:localhost:2345 ${DBLAB_SSH_HOST} -i ~/.ssh/id_rsa
+ssh -f -N -L 2344:localhost:2345 -i ~/.ssh/id_rsa ${DBLAB_SSH_HOST}
 
 dblab init --url http://127.0.0.1:2344 --token ${DBLAB_TOKEN} --environment-id ${DBLAB_ENVIRONMENT}
+
+dblab instance status
 
 dblab_info=$(dblab clone create --username ${DBLAB_USER} --password ${DBLAB_PASSWORD})
 
 port=$(echo $dblab_info | jq -r .db.port)
 
+echo $dblab_info
+echo
+echo "Opening port forwarding on port ${port}"
+
 # This blocks and opens port forwarding for Postgres
-ssh -N -L 5432:localhost:$port ${DBLAB_SSH_HOST} -i ~/.ssh/id_rsa
+ssh -N -L 5432:localhost:$port -i ~/.ssh/id_rsa ${DBLAB_SSH_HOST}
 
