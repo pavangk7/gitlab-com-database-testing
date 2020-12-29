fc592c883a79f5c86faa68a950c9614420327d39
Andreas Brandl <abrandl@gitlab.com>
2020-12-29 22:37:51 +0100

    SAVEPOINT

--- docker/gitlab/entrypoint.sh
+++ docker/gitlab/entrypoint.sh
@@ -6,12 +6,11 @@
 # Inspired by https://dev.to/andre/docker-restricting-in--and-outbound-network-traffic-67p
 #
 # The following allows in- and outbound traffic
-# within a certain `CIDR` (default: `10.128.0.0/24`),
+# within certain CIDRs,
 # but blocks all other network traffic.
-#
-ACCEPT_CIDR=${ALLOWED_CIDR:-10.128.0.0/24}
 
-iptables -A INPUT -s $ACCEPT_CIDR -j ACCEPT
+iptables -A INPUT -s 172.16.0.0/12 -j ACCEPT
+iptables -A INPUT -s 10.128.0.0/24 -j ACCEPT
 iptables -A INPUT -j DROP
 iptables -A OUTPUT -d $ACCEPT_CIDR -j ACCEPT
 iptables -A OUTPUT -j DROP
