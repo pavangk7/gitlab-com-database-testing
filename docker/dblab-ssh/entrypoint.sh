1ac42e03b45bec321e0314d063d266f0c8b73308
Andreas Brandl <abrandl@gitlab.com>
2020-12-29 20:08:17 +0100

    SAVEPOINT

--- docker/dblab-ssh/entrypoint.sh
+++ docker/dblab-ssh/entrypoint.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+set -e
 
 mkdir ~/.ssh
 echo "${DBLAB_SSH_KEY}" > ~/.ssh/id_rsa
@@ -8,4 +10,10 @@ ssh -f -N -L 2344:localhost:2345 ${DBLAB_SSH_HOST} -i ~/.ssh/id_rsa
 
 dblab init --url http://127.0.0.1:2344 --token ${DBLAB_TOKEN} --environment-id ${DBLAB_ENVIRONMENT}
 
-sh -c "$@"
+dblab_info=$(dblab clone create --username ${DBLAB_USER} --password ${DBLAB_PASSWORD})
+
+port=$(echo $dblab_info | jq -r .db.port)
+
+# This blocks and opens port forwarding for Postgres
+ssh -N -L 5432:localhost:$port ${DBLAB_SSH_HOST} -i ~/.ssh/id_rsa
+
---
+++ docker/gitlab/prepare.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cp config/gitlab.yml.example config/gitlab.yml
+sed -i 's/bin_path: \/usr\/bin\/git/bin_path: \/usr\/local\/bin\/git/' config/gitlab.yml
+
+cp config/database.yml.postgresql config/database.yml
+
+# Set user to a non-superuser to ensure we test permissions
+sed -i 's/username: root/username: gitlab/g' config/database.yml
+
+sed -i 's/localhost/postgres/g' config/database.yml
+sed -i "s/username: git/username: ${DBLAB_USER}/g" config/database.yml
+sed -i "s/password: git/password: ${DBLAB_PASSWORD}/g" config/database.yml
+
+cp config/cable.yml.example config/cable.yml
+sed -i 's|url:.*$|url: redis://redis:6379|g' config/cable.yml
+
+cp config/resque.yml.example config/resque.yml
+sed -i 's|url:.*$|url: redis://redis:6379|g' config/resque.yml
+
+cp config/redis.cache.yml.example config/redis.cache.yml
+sed -i 's|url:.*$|url: redis://redis:6379/10|g' config/redis.cache.yml
+
+cp config/redis.queues.yml.example config/redis.queues.yml
+sed -i 's|url:.*$|url: redis://redis:6379/11|g' config/redis.queues.yml
+
+cp config/redis.shared_state.yml.example config/redis.shared_state.yml
+sed -i 's|url:.*$|url: redis://redis:6379/12|g' config/redis.shared_state.yml
