f323971310ecfdb986afe6198c053d68ef79f009
Andreas Brandl <abrandl@gitlab.com>
2020-12-29 20:23:31 +0100

    SAVEPOINT

--- docker/dblab-ssh/entrypoint.sh
+++ docker/dblab-ssh/entrypoint.sh
@@ -6,6 +6,8 @@ mkdir ~/.ssh
 echo "${DBLAB_SSH_KEY}" > ~/.ssh/id_rsa
 echo "${DBLAB_HOST_KEYS}" > ~/.ssh/known_hosts
 
+cat ~/.ssh/known_hosts
+
 ssh -f -N -L 2344:localhost:2345 ${DBLAB_SSH_HOST} -i ~/.ssh/id_rsa
 
 dblab init --url http://127.0.0.1:2344 --token ${DBLAB_TOKEN} --environment-id ${DBLAB_ENVIRONMENT}
