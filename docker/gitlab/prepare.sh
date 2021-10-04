994774706638ed324ce61b8b6b639655bca18b31
Heinrich Lee Yu <heinrich@gitlab.com>
2021-10-04 15:35:05 +0800

    Fix failure due to missing example files
    The example files were removed leaving only resque.yml.example. That was
    done in https://gitlab.com/gitlab-org/gitlab/-/commit/b3c5c99600ade360199931904fcb3844f6fa9391
    because all of them are very similar.

--- docker/gitlab/prepare.sh
+++ docker/gitlab/prepare.sh
@@ -31,11 +31,11 @@ sed -i 's|url:.*$|url: redis://redis:6379|g' config/cable.yml
 cp config/resque.yml.example config/resque.yml
 sed -i 's|url:.*$|url: redis://redis:6379|g' config/resque.yml
 
-cp config/redis.cache.yml.example config/redis.cache.yml
+cp config/resque.yml.example config/redis.cache.yml
 sed -i 's|url:.*$|url: redis://redis:6379/10|g' config/redis.cache.yml
 
-cp config/redis.queues.yml.example config/redis.queues.yml
+cp config/resque.yml.example config/redis.queues.yml
 sed -i 's|url:.*$|url: redis://redis:6379/11|g' config/redis.queues.yml
 
-cp config/redis.shared_state.yml.example config/redis.shared_state.yml
+cp config/resque.yml.example config/redis.shared_state.yml
 sed -i 's|url:.*$|url: redis://redis:6379/12|g' config/redis.shared_state.yml
