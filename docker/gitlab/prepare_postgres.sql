0649b64e7bb0018586273f5bf4906b6e54be01bc
Simon Tomlinson <stomlinson@gitlab.com>
2022-01-20 16:50:48 +0000

    Revert "Merge branch 'ab/auto-explain' into 'master'"
    This reverts merge request !78

--- docker/gitlab/prepare_postgres.sql
+++
@@ -1,9 +0,0 @@
--- Applied to a thin clone before any testing
-
-LOAD 'auto_explain';
-ALTER SYSTEM SET auto_explain.log_min_duration='1s';
-ALTER SYSTEM SET auto_explain.log_triggers='on';
-ALTER SYSTEM SET auto_explain.log_buffers='on';
-ALTER SYSTEM SET auto_explain.log_analyze='on';
-
-SELECT pg_reload_conf();
