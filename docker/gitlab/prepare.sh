668775301ef9854d7a0db4b74bafcfdf09312d3b
Simon Tomlinson <stomlinson@gitlab.com>
2022-02-23 10:40:27 -0600

    Ensure that Gitlab.com? is true during testing

--- docker/gitlab/prepare.sh
+++ docker/gitlab/prepare.sh
@@ -54,6 +54,7 @@ if test "$VALIDATION_PIPELINE"; then
   git am < /gitlab/patches/testing/Exception-Raised-in-Migration.patch
   git am < /gitlab/patches/testing/Add-a-migration-that-takes-five-seconds.patch
   git am < /gitlab/patches/testing/Migration-inheriting-Gitlab-Database-Migration.patch
+  git am < /gitlab/patches/testing/Migration-that-requires-Gitlab-com-true.patch
 fi
 
 ### REDIS
